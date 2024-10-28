cde065eef1640404a0b56883fed0961beca0f263
C. SHEN <chunhua@me.com>
2024-10-28 20:08:32 +1030

    CS: commit save

--- functions/arxiv.fish
+++ functions/arxiv.fish
@@ -13,12 +13,30 @@ function arxiv -d "downalod arxiv papers. `axs' is needed."
 
     # using axs to download. 
     # Install:  pipx install git+https://github.com/cshen/arxiv_download   
+    
     echo "command: axs get -d $HOME/Downloads/_arXiv $ID"
     
-    axs get -d $HOME/Downloads/_arXiv $ID 
+    if __is_macOS 
+        set fn ( axs get -d $HOME/Downloads/_arXiv $ID | grep -i "Article saved" |  \
+            rev | awk -F/ '{print $1}'| rev | awk -F. '{print $1}' )
+        
+       echo The downloaded file is $fn.pdf in $HOME/Downloads/_arXiv \(file path is copied to clipboard\) 
+       echo $HOME/Downloads/_arXiv/$fn.pdf | pbcopy  
+    else
+        axs get -d $HOME/Downloads/_arXiv $ID  
+    end
+
     arxiv_bib $ID $HOME/Downloads/_arXiv/_cs_arxiv.bib
-    
+
     # ls -l --color -t $HOME/Downloads/_arXiv
     # type -d detox && detox $HOME/Downloads/_arXiv/*
 end
 
+
+function __is_macOS
+    if test (uname) = "Darwin" 
+        return 0
+    else
+        return 1
+    end
+end
